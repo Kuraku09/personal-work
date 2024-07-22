fd2b392b743a43073de3a53f5418908fbb5f36e9
Kuraku09 <alex89090456@gmail.com>
2024-07-22 15:36:55 +0800

    test

--- login.js
+++
@@ -1,9 +0,0 @@
-const myEmail = document.querySelector('.my-email');
-const myPassword = document.querySelector('.my-password');
-const myLogin = document.querySelector('.my-login');
-console.log(myEmail);
-myLogin.addEventListener('click', () => {
-    if (myPassword.value === 'null' || myPassword.value === 'null') {
-        alert("請輸入正確的信箱和密碼");
-    } 
-})
